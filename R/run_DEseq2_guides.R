321d2a295338cb4fe2b0a6b2db567e4cebec6e79
FloWuenne <flowuenne@gmail.com>
2018-11-05 15:28:44 -0500

    Added new function to calculate DESeq2 logFold changes for sgRNA counts.

---
+++ R/run_DEseq2_guides.R
@@ -0,0 +1,66 @@
+#' A function to create a reference fasta and gtf for plasmid + gRNA fusion for CROP-seq screens
+#'
+#' This function takes as input a table containing the gRNA sequences and will output a .fasta and a .gtf file \
+#' containing the merged plasmid + gRNA sequences and the gtf description to add them to a reference genome.
+#' @param sgRNA_table The table containing the sgRNA sequences and annotation.
+#' @export
+#' @examples
+#' run_deseq2_on_guides()
+
+run_deseq2_on_guides <- function(design_matrix,
+                                 sgRNA_count_table,
+                                 design_column = "distribution")
+  {
+
+  ## Specify required libraries
+  require(DESeq2)
+  require(data.table)
+
+  ## Load design matrix
+  design <- read.table(design_matrix,
+                       sep="\t",
+                       header=T,
+                       row.names= 1,
+                       quote = "")
+
+  design <- subset(design,distribution != "library")
+
+  ## Load count tables calculated using MAGECK count
+  counts <- fread(sgRNA_count_table,
+                  sep="\t",
+                  header=T,
+                  quote = "")
+
+  counts_filtered <- counts
+
+  rownames(counts_filtered) <- counts_filtered$sgRNA
+  counts_filtered <- counts_filtered[,-c(1,2,5)]
+
+  ## Create DEseq2 object
+  dds <- DESeqDataSetFromMatrix(countData = counts_filtered,
+                                colData = design,
+                                design = ~ design_column)
+
+  dds$distribution <- factor(dds$distribution, levels = c("BOTTOM","TOP"))
+
+  dds <- DESeq(dds)
+  res <- results(dds)
+
+  ## Add sgRNA and locus information to the DESeq2 results
+  res$sgRNA <- counts$sgRNA
+  res$locus <- counts$Gene
+
+  ## Find all the guides that have NA in their logFold change
+  res_na <- res %>%
+    subset(is.na(log2FoldChange))
+
+  ## Filter out the sgRNA that have NA
+  res <- res %>%
+    subset(!is.na(log2FoldChange))
+
+  ## Order results by p-value
+  resOrdered <- res[order(res$pvalue),]
+
+  return(resOrdered)
+
+}
--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # cropseq.analysis 
-# A collection of functions to prepare and analyze single cell CRISPR perturbation screens using the CROP-seq approach
+
+## A collection of functions to prepare and analyze single cell CRISPR perturbation screens using the CROP-seq approach or pooled CRISPR screens
 
 ## Installation
 Install the package using the devtools install_github function as shown below:
 
 library(devtools)
 
-install_github("FloWuenne/scFunctions")
+install_github("FloWuenne/cropseq.analysis")
 
 ## Functions 
 
